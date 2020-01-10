63be42183768afb2ee33c055f18c52fd312a5705
Victor Barbosa <vicldb99@gmail.com>
2020-01-09 23:31:25 -0300

    components

---
+++ src/components/Header.js
@@ -0,0 +1,8 @@
+import React, { Component } from 'react';
+
+class Header extends Component{
+    render(){
+        return <header className="text-center"><h1>Header</h1></header>
+    }
+}
+export default Header;
---
+++ src/components/Main.js
@@ -0,0 +1,8 @@
+import React, { Component } from 'react';
+
+class Main extends Component{
+    render(){
+        return <main><h1 className="text-center">Main</h1></main>
+    }
+}
+export default Main;
